16ce7f0b67b62e4958c57e66f6beea491593abc0
Nogmanov Ildar <nogmanov@gmail.com>
2018-04-21 20:30:31 +0300

    Add form & some styles

--- src/components/Party/party.js
+++ src/components/Party/party.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FormGroup, ControlLabel } from "react-bootstrap";
+import { Button, FormGroup, ControlLabel, FormControl } from "react-bootstrap";
+import "./Party.css";
 
 const Party = () => (
   <div className="App">
@@ -9,9 +10,55 @@ const Party = () => (
         <h1 className="App-title">Party Booster</h1>
       </Link>
     </header>
-    <FormGroup controlId="form">
-      <ControlLabel>How many people?</ControlLabel>
-    </FormGroup>
+    <form>
+      <FormGroup controlId="people">
+        <ControlLabel>How many people?</ControlLabel>
+        <FormControl componentClass="select" placeholder="select">
+          <option value="1">Less than 5</option>
+          <option value="2">6-10</option>
+          <option value="3">11-15</option>
+          <option value="4">More than 16</option>
+        </FormControl>
+      </FormGroup>
+
+      <FormGroup controlId="type">
+        <ControlLabel>What type of party?</ControlLabel>
+        <FormControl componentClass="select" placeholder="select">
+          <option value="1">Meeting with friends</option>
+          <option value="2">Birthday</option>
+          <option value="3">Wedding</option>
+          <option value="4">Graduation</option>
+          <option value="5">Corporate meeting</option>
+        </FormControl>
+      </FormGroup>
+
+      <FormGroup controlId="place">
+        <ControlLabel>Where do plan to go?</ControlLabel>
+        <FormControl componentClass="select" placeholder="select">
+          <option value="1">At home</option>
+          <option value="2">Cafe\Bar</option>
+          <option value="3">Club</option>
+          <option value="4">Other</option>
+        </FormControl>
+      </FormGroup>
+
+      <FormGroup controlId="todo">
+        <ControlLabel>What do you plan to do?</ControlLabel>
+        <FormControl componentClass="select" placeholder="select">
+          <option value="1">Drinking</option>
+          <option value="2">Smoking</option>
+          <option value="3">Playing games</option>
+          <option value="4">Listening to music</option>
+          <option value="5">Dancing</option>
+        </FormControl>
+      </FormGroup>
+
+      <FormGroup controlId="ideal">
+        <ControlLabel>Describe your ideal party</ControlLabel>
+        <FormControl type="text" placeholder="Enter text" />
+      </FormGroup>
+      <Button type="submit">Submit</Button>
+    </form>
   </div>
 );
 
