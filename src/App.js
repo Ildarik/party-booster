be7cf6920bffd82216376ddd60a413936709374e
Nogmanov Ildar <nogmanov@gmail.com>
2018-04-21 15:43:04 +0300

    Add buttons & icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import logo from "./SMirC-party.svg";
+import { Button } from "react-bootstrap";
+import logo from "./images/SMirC-party.svg";
 import "./App.css";
 
 class App extends Component {
@@ -10,6 +11,24 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">PartyBooster</h1>
         </header>
+        <main>
+          <Button className="button" bsStyle="primary" bsSize="large">
+            <span className="get-random btn-icon" />
+            <span>Get Random PARTY!</span>
+          </Button>
+          <Button className="button" bsStyle="success" bsSize="large">
+            <span className="create btn-icon" />
+            <span>Create your PARTY!</span>
+          </Button>
+          <Button className="button" bsStyle="warning" bsSize="large">
+            <span className="organization btn-icon" />
+            <span>All for organization</span>
+          </Button>
+          <Button className="button" bsStyle="info" bsSize="large">
+            <span className="buddy btn-icon" />
+            <span>Drunk buddy</span>
+          </Button>
+        </main>
       </div>
     );
   }
