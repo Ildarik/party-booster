00eb8feb43d89886e0c79f54f14194e719ab8300
Nogmanov Ildar <nogmanov@gmail.com>
2018-04-21 20:59:46 +0300

    Rename values

--- src/components/Party/party.js
+++ src/components/Party/party.js
@@ -10,46 +10,46 @@ const Party = () => (
         <h1 className="App-title">Party Booster</h1>
       </Link>
     </header>
-    <form>
+    <form onSubmit={handleSubmit()}>
       <FormGroup controlId="people">
         <ControlLabel>How many people?</ControlLabel>
         <FormControl componentClass="select" placeholder="select">
-          <option value="1">Less than 5</option>
-          <option value="2">6-10</option>
-          <option value="3">11-15</option>
-          <option value="4">More than 16</option>
+          <option value="less5">Less than 5</option>
+          <option value="sixten">6-10</option>
+          <option value="elevenfifteen">11-15</option>
+          <option value="more16">More than 16</option>
         </FormControl>
       </FormGroup>
 
       <FormGroup controlId="type">
         <ControlLabel>What type of party?</ControlLabel>
         <FormControl componentClass="select" placeholder="select">
-          <option value="1">Meeting with friends</option>
-          <option value="2">Birthday</option>
-          <option value="3">Wedding</option>
-          <option value="4">Graduation</option>
-          <option value="5">Corporate meeting</option>
+          <option value="meeting">Meeting with friends</option>
+          <option value="birthday">Birthday</option>
+          <option value="wedding">Wedding</option>
+          <option value="graduation">Graduation</option>
+          <option value="corporate">Corporate meeting</option>
         </FormControl>
       </FormGroup>
 
       <FormGroup controlId="place">
         <ControlLabel>Where do plan to go?</ControlLabel>
         <FormControl componentClass="select" placeholder="select">
-          <option value="1">At home</option>
-          <option value="2">Cafe\Bar</option>
-          <option value="3">Club</option>
-          <option value="4">Other</option>
+          <option value="home">At home</option>
+          <option value="cafe">Cafe\Bar</option>
+          <option value="club">Club</option>
+          <option value="other">Other</option>
         </FormControl>
       </FormGroup>
 
       <FormGroup controlId="todo">
         <ControlLabel>What do you plan to do?</ControlLabel>
         <FormControl componentClass="select" placeholder="select">
-          <option value="1">Drinking</option>
-          <option value="2">Smoking</option>
-          <option value="3">Playing games</option>
-          <option value="4">Listening to music</option>
-          <option value="5">Dancing</option>
+          <option value="drinking">Drinking</option>
+          <option value="smoking">Smoking</option>
+          <option value="gaming">Playing games</option>
+          <option value="music">Listening to music</option>
+          <option value="dancing">Dancing</option>
         </FormControl>
       </FormGroup>
 
