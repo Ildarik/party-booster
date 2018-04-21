0f54d02ce22d87be2ff3b779a4f875b9d33f9ce3
Nogmanov Ildar <nogmanov@gmail.com>
2018-04-21 19:20:13 +0300

    Add routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "normalize.css";
 import "./index.css";
 import App from "./App";
+import Party from "./components/Party";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/create" component={Party} />
+    </div>
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
