806a02e1dd63516bb8eeeb1f32aa0c35d01745a9
Nogmanov Ildar <nogmanov@gmail.com>
2018-04-21 15:59:22 +0300

    Align buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button } from "react-bootstrap";
+import { Grid, Row, Col, Button } from "react-bootstrap";
 import logo from "./images/SMirC-party.svg";
 import "./App.css";
 
@@ -12,22 +12,36 @@ class App extends Component {
           <h1 className="App-title">PartyBooster</h1>
         </header>
         <main>
-          <Button className="button" bsStyle="primary" bsSize="large">
-            <span className="get-random btn-icon" />
-            <span>Get Random PARTY!</span>
-          </Button>
-          <Button className="button" bsStyle="success" bsSize="large">
-            <span className="create btn-icon" />
-            <span>Create your PARTY!</span>
-          </Button>
-          <Button className="button" bsStyle="warning" bsSize="large">
-            <span className="organization btn-icon" />
-            <span>All for organization</span>
-          </Button>
-          <Button className="button" bsStyle="info" bsSize="large">
-            <span className="buddy btn-icon" />
-            <span>Drunk buddy</span>
-          </Button>
+          <Grid>
+            <Row>
+              <Col xs={12} md={2} mdOffset={3}>
+                <Button className="button" bsStyle="primary" bsSize="large">
+                  <span className="get-random btn-icon" />
+                  <span>Get Random PARTY!</span>
+                </Button>
+              </Col>
+              <Col xs={12} md={2} mdOffset={1}>
+                <Button className="button" bsStyle="success" bsSize="large">
+                  <span className="create btn-icon" />
+                  <span>Create your PARTY!</span>
+                </Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} md={2} mdOffset={3}>
+                <Button className="button" bsStyle="warning" bsSize="large">
+                  <span className="organization btn-icon" />
+                  <span>All for organization</span>
+                </Button>
+              </Col>
+              <Col xs={12} md={2} mdOffset={1}>
+                <Button className="button" bsStyle="info" bsSize="large">
+                  <span className="buddy btn-icon" />
+                  <span>Drunk buddy</span>
+                </Button>
+              </Col>
+            </Row>
+          </Grid>
         </main>
       </div>
     );
